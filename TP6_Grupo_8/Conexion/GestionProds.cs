0afca84393a5fb711234b8d6d80b9ccd99cf3c9f
Keys4Ever <cubillagonzalo@outlook.com>
2025-05-10 22:26:51 -0300

    aporte 5

---
+++ TP6_Grupo_8/Conexion/GestionProds.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using TP6_Grupo_8.Conexion;
+using TP6_Grupo_8;
+
+namespace tp6intento2
+{
+    public class GestionProds
+    {
+        public GestionProds()
+        {
+            //por defecto.
+        }
+
+        private DataTable ObtenerTabla (string nombreTabla, string consultaSQL)
+        {
+            DataSet dataSet = new DataSet();
+            AccesoDatos datos = new AccesoDatos();
+            SqlDataAdapter sqlDataAdapter = datos.ObtenerAdapdator(consultaSQL);
+            sqlDataAdapter.Fill(dataSet, nombreTabla);
+            return dataSet.Tables[nombreTabla];
+        }
+
+        public DataTable ObtenerTodosLosDatos ()
+        {
+            return ObtenerTabla("Productos", "SELECT IdProducto, NombreProducto, CantidadPorUnidad, PrecioUnidad FROM Productos");
+        }
+
+       
+    }
+}
